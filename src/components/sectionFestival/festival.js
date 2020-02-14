35a2f83f446e1102dacf241df80ff564ccf1dca7
Llorens <llorensmartinezmartinez@gmail.com>
2020-02-14 17:12:21 +0100

    Festival finish add agradecimientos

--- src/components/sectionFestival/festival.js
+++ src/components/sectionFestival/festival.js
@@ -10,17 +10,16 @@ const festival = ({title}) => {
       <div className="festivalContainer">
         <h2>{title}</h2>
         <img src="images/festivalNunes.png" alt="Festival Nunes"/>
-        <p>El <b>Ateneo de liertad de Gracia</b> y <b>Adolfo Quibus</b> Producciones Cinematograficas S.L,
-          presentan el 9º Festival Internacional Nunes, dedicado al gran director y amigo  Jose María Nunes.
-        </p>
+        <p>Ha finalizado el 9 FESTIVAL INTERNACIONAL DE CINE NUNES con la entrega
+          de premios el jurado tras larga deliberacion ha decidido de forma inapelable:</p>
         <div className="festivalInfoContainer">
-            <p>Martes 10 - 14 Diciembre 2019, 18:00 PM</p>
-            <p>Ateneo Libertario</p>
-            <p>Carrer de l'Alzina 5</p>
-            <p>08033 Barcelona</p>
-            <p>España</p>
-          </div>
-        <Link id="buttonProgram" to={"/program"}>PROGRAMACION</Link>
+            <p><b>Mejor Pelicula de Ficcion:</b> “Jump” de Liberto Rabal</p>
+            <p><b>Mejor Documental:</b> “Hijas de Cynisca” de Beatriz Carretero</p>
+            <p><b>Nunes de Honor:</b> Carlos Mir</p>
+        </div>
+        <br/>
+        <p>Felicidades a los ganadores y a los seleccionados por el extraordinario nivel de sus películas. Seguiremos informando.</p>
+        <Link id="buttonProgram" to={"/program"}>AGRADECIMIENTOS</Link>
       </div>
     </section>
   );
--- src/pages/program/program.js
+++ src/pages/program/program.js
@@ -7,19 +7,20 @@ import './program.css'
 const program = () => {
   return (
     <section className="black section sectionProgram">
-      <h4>Martes 10 de Diciembre</h4>
-      <p><span className="red">19:00</span> INAGURACION del festival y balance de los 9 años transcurridos</p>
-      <h4>Miercoles 11 de Diciembre</h4>
-      <p><span className="red">18:00</span> Jump, Liberto Rabal, 65 min</p>
-      <p><span className="red">19:30</span> Call of Dreams, Ran Slavin, 81 min</p>
-      <h4>Jueves 12 de Diciembre</h4>
-      <p><span className="red">18:00</span> La Estrella, Francisco Martin, 65 min</p>
-      <p><span className="red">19:30</span> Hijas de Cynisca, Beatriz Carretero, 96 min</p>
-      <h4>Viernes 13 de Diciembre</h4>
-      <p><span className="red">18:00</span> Lors Of The Body, Marc Nadal, 66 min</p>
-      <p><span className="red">19:30</span> Apostata de Hugo Cobo, 62min</p>
-      <h4>Sabado 14 de Diciembre</h4>
-      <p><span className="red">19:00</span>Entrega de PREMIOS y clausura del festival</p>
+      <h5>Hijas de Cynisca: Mejor documental</h5>
+
+      <iframe width="100%" height="50%"
+        title="Hijas de Cynisca"
+        src="http://www.youtube.com/embed/3TVeUaWyVu8">
+      </iframe>
+
+      <h5>JUMP: Mejor largometraje de ficción</h5>
+
+      <iframe width="100%" height="50%"
+        title="Jump"
+        src="http://www.youtube.com/embed/Jj04wLYAPjc">
+      </iframe>
+
       <Link id="buttonFestival" to={'/'}>Back</Link>
     </section>
   );
